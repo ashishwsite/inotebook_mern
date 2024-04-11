6e0bd8b06674a9b9267fd956ee197245de5520f8
ashish <1508ramashish@gmail.com>
2024-04-11 17:36:49 +0530

    update

--- inotebook/src/components/Singup.js
+++ inotebook/src/components/Singup.js
@@ -11,7 +11,7 @@ const Signup = (props) => {
       const handleSubmit = async (e) => {
         e.preventDefault();
         console.log("handle sumit is call ")
-        const response = await fetch("http://localhost:5000/api/auth/createuser", {
+        const response = await fetch("https://inotebook-backend-xi.vercel.app/api/auth/createuser", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
